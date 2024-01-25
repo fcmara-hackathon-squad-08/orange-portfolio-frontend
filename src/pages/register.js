337fac94ccce71f562daeca1ba46db7720401319
Alan Vargas <alanvargasenf@gmail.com>
2024-01-25 00:35:49 -0300

    adding password visibility button and form validation in JS

---
+++ src/pages/register.js
@@ -0,0 +1,14 @@
+//ìcone de vizibilidade da senha
+const password = document.getElementById('password');
+const icon = document.getElementById ('icon');
+
+function showHide(){
+    if(password.type === 'password'){
+        password.setAttribute('type', 'text');
+        icon.classList.add('hide');
+    }else{
+        password.setAttribute('type', 'password');
+        icon.classList.remove('hide')
+    }
+}
+
