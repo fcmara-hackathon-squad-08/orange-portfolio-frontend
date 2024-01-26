2cce15016e5510b942574dfdc20ff5250abf61f5
Kauã Steliano <stelianok@gmail.com>
2024-01-26 19:02:48 -0300

    Now selecting a valid image hides the placeholder text and icon on the button.

--- public/src/pages/my-portfolio.js
+++ public/src/pages/my-portfolio.js
@@ -1,26 +1,35 @@
-//Controls the addProject Modal
 const openModalButton = document.getElementById("add-project-button");
 const modalContainer = document.getElementById("add-project-modal");
 const closeModalButton = document.getElementById("cancel-add-project-button");
 
-//Controls display of the selected image
 let selectedImage = document.getElementById("selected-image");
 const submitImageCardContent = document.getElementById("submit-image-card-content");
 
 function uploadImage(input) {
+  hideSubmitImageCardContent()
   if (input.files && input.files[0]) {
     let reader = new FileReader();
+
 
     reader.onload = function (e) {
       selectedImage.src = e.target.result
     }
 
     reader.readAsDataURL(input.files[0]);
+
   }
 }
 
 function chooseFile() {
   document.getElementById("fileInput").click();
+}
+
+function hideSubmitImageCardContent() {
+  submitImageCardContent.classList.add('hidden');
+}
+
+function showSubmitImageCardContent() {
+  submitImageCardContent.classList.remove('hidden');
 }
 
 
