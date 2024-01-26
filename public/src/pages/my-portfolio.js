b71f49e3556c68dfe664abcd6952e1784571b27c
Kauã Steliano <stelianok@gmail.com>
2024-01-26 17:04:52 -0300

    Now a project image can be selected an shown on screen.

--- public/src/pages/my-portfolio.js
+++ public/src/pages/my-portfolio.js
@@ -1,6 +1,27 @@
+//Controls the addProject Modal
 const openModalButton = document.getElementById("add-project-button");
 const modalContainer = document.getElementById("add-project-modal");
 const closeModalButton = document.getElementById("cancel-add-project-button");
+
+//Controls display of the selected image
+let selectedImage = document.getElementById("selected-image");
+const submitImageCardContent = document.getElementById("submit-image-card-content");
+
+function uploadImage(input) {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+
+    reader.onload = function (e) {
+      selectedImage.src = e.target.result
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function chooseFile() {
+  document.getElementById("fileInput").click();
+}
 
 
 openModalButton.addEventListener('click', () => {
@@ -10,3 +31,4 @@ openModalButton.addEventListener('click', () => {
 closeModalButton.addEventListener('click', () => {
   modalContainer.classList.remove('show');
 })
+
