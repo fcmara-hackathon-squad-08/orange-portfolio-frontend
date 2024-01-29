eaeab598be1507247ba86e46d50550c9f2751e06
Kauã Steliano <stelianok@gmail.com>
2024-01-29 01:35:37 -0300

    updated imports with correct extensions

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // Material Design components
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
-import '@material/web/iconbutton/filled-icon-button';
+
+import '@material/web/iconbutton/filled-icon-button.js';
+import '@material/web/icon/icon.js';
+
 import '@material/web/ripple/ripple.js';
 
-import '@material/web/textfield/outlined-text-field';
-import '@material/web/textfield/filled-text-field';
+import '@material/web/textfield/outlined-text-field.js';
+import '@material/web/textfield/filled-text-field.js';
 
-import '@material/web/chips/chip-set';
-import '@material/web/chips/suggestion-chip';
+import '@material/web/chips/chip-set.js';
+import '@material/web/chips/suggestion-chip.js';
 
 import '@material/web/menu/menu.js';
-import '@material/web/menu/menu-item';
+import '@material/web/menu/menu-item.js';
