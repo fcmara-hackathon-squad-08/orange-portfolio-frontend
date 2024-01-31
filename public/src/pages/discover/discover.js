b739e7c64235109ac5a6a36b3bec3afd00cff6a1
Alan Vargas <alanvargasenf@gmail.com>
2024-01-31 00:24:30 -0300

    CSS and HTML update and their responsiveness, validations in JS

--- public/src/pages/discover/discover.js
+++ public/src/pages/discover/discover.js
@@ -1,47 +1,10 @@
-// Modal 01
-function openModal(){
-    const modal = document.getElementById('modal-window');
+// Função genérica para abrir modal
+function openModal(modalId){
+    const modal = document.getElementById(modalId);
     modal.classList.add('open-modal');
 
     modal.addEventListener('click', (e) => {
-        if(e.target.id == 'close-modal' || e.target.id == 'modal-window'){
-            modal.classList.remove('open-modal')
-        }
-    })
-}
-
-
-// Modal 02
-function openModal2(){
-    const modal = document.getElementById('modal-window-02');
-    modal.classList.add('open-modal');
-
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'close-modal' || e.target.id == 'modal-window-02'){
-            modal.classList.remove('open-modal')
-        }
-    })
-}
-
-// Modal 03
-function openModal3(){
-    const modal = document.getElementById('modal-window-03');
-    modal.classList.add('open-modal');
-
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'close-modal' || e.target.id == 'modal-window-03'){
-            modal.classList.remove('open-modal')
-        }
-    })
-}
-
-// Modal 04
-function openModal4(){
-    const modal = document.getElementById('modal-window-04');
-    modal.classList.add('open-modal');
-
-    modal.addEventListener('click', (e) => {
-        if(e.target.id == 'close-modal' || e.target.id == 'modal-window-04'){
+        if(e.target.id == 'close-modal' || e.target.id == modalId){
             modal.classList.remove('open-modal')
         }
     })
--- public/src/pages/register/register.js
+++ public/src/pages/register/register.js
@@ -1,104 +1,41 @@
 // Vizibilidade da senha
-const password = document.getElementById('password');
-const passwordVisibilityIcon = document.getElementById('passwordVisibilityIcon');
-
-function TogglePasswordVisibility() {
-  if (password.type === 'password') {
-    password.setAttribute('type', 'text');
-    passwordVisibilityIcon.classList.add('hide');
+document.querySelector('md-icon-button').addEventListener('click', () => {
+  const input = document.querySelector('#password');
+  const eye = document.querySelector('#eye');
+  const eyeOff = document.querySelector('#eyeOff');
+  if (input.type === "password") {
+    input.type = "text";
+    eye.style.display = "none";
+    eyeOff.style.display = "block";
   } else {
-    password.setAttribute('type', 'password');
-    passwordVisibilityIcon.classList.remove('hide');
-  }
-}
-
-// Validação do formulário
-
-const firstname = document.querySelector('#firstname');
-const labelFirstname = document.querySelector('#labelFirstname');
-let validFirstname = false;
-
-const lastname = document.querySelector('#lastname');
-const labelLastname = document.querySelector('#labelLastname');
-let validLastname = false;
-
-const emailAdd = document.querySelector('#email-add');
-const labelEmail = document.querySelector('#labelEmail');
-let validEmail = false;
-
-const passwordAdd = document.querySelector('#password');
-const labelPassword = document.querySelector('#labelPassword');
-let validPassword = false;
-
-firstname.addEventListener('keyup', () => {
-  if (firstname.value.length <= 2) {
-    labelFirstname.setAttribute('style', 'color: red');
-    labelFirstname.innerHTML = ('Nome * Insira no mínimo 3 caracteres');
-    firstname.setAttribute('style', 'border-color: red');
-    validFirstname = false;
-  } else {
-    labelFirstname.setAttribute('style', 'color: green');
-    labelFirstname.innerHTML = ('Nome *');
-    firstname.setAttribute('style', 'border-color: green');
-    validFirstname = true;
+    input.type = "password";
+    eye.style.display = "block";
+    eyeOff.style.display = "none";
   }
 });
 
-lastname.addEventListener('keyup', () =>{
-    if(lastname.value.length <= 3){
-        labelLastname.setAttribute('style', 'color: red');
-        labelLastname.innerHTML = ('Sobrenome * Insira no mínimo 4 caracteres');
-        lastname.setAttribute('style', 'border-color: red');
-        validLastname = false;
-    }else{
-        labelLastname.setAttribute('style', 'color: green');
-        labelLastname.innerHTML = ('Sobrenome *');
-        lastname.setAttribute('style', 'border-color: green');
-        validLastname = true;
-    }
-});
+const register = (event) => {
+  // Impede que a página seja recarregada
+  event.preventDefault();
 
-emailAdd.addEventListener('keyup', () => {
-  if (emailAdd.value.length <= 5) {
-    labelEmail.setAttribute('style', 'color: red');
-    labelEmail.innerHTML = ('Email address * Insira um email válido');
-    emailAdd.setAttribute('style', 'border-color: red');
-    validEmail = false;
-  } else {
-    labelEmail.setAttribute('style', 'color: green');
-    labelEmail.innerHTML = ('Email address');
-    emailAdd.setAttribute('style', 'border-color: green');
-    validEmail = true;
+  const firstname = document.getElementById('fristname').value;
+  const lastname = document.getElementById('lastname').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Validação dos campos de entrada
+  if (!firstname || !lastname || !email || !password) {
+    alert('Por favor, preencha todos os campos.');
+    return;
   }
-});
 
-passwordAdd.addEventListener('keyup', () => {
-  if (passwordAdd.value.length <= 5) {
-    labelPassword.setAttribute('style', 'color: red');
-    labelPassword.innerHTML = ('Password * Insira no mínimo 6 caracteres');
-    passwordAdd.setAttribute('style', 'border-color: red');
-    validPassword = false;
-  } else {
-    labelPassword.setAttribute('style', 'color: green');
-    labelPassword.innerHTML = ('Password');
-    passwordAdd.setAttribute('style', 'border-color: green');
-    validPassword = true;
-  }
-});
+  // Exibir a mensagem de sucesso
+  const successMessage = document.getElementById('success');
+  successMessage.style.display = 'block';
 
-// Mensagem de confirmação de cadastro
-const success = document.querySelector('#success');
-
-// Função de cadastro
-
-function register() {
-  if (validFirstname && validLastname && validEmail && validPassword) {
-    success.setAttribute('style', 'visibility: visible');
-
-    setTimeout(() => {
-      window.location.href = 'login.html';
-    }, 2000);
-  } else {
-    // alert('Preencha os dados corretamente!');
-  }
+  // Redirecionar para a tela de login após 2 segundos
+  setTimeout(() => {
+    successMessage.style.display = 'none';
+    window.location.href = '../login/index.html';
+  }, 2000);
 }
