87f222c6c682f4c02f72631dfe8efbda45e17ac8
Alan Vargas <alanvargasenf@gmail.com>
2024-01-31 13:35:02 -0300

    inclusion of menu functionality for mobile

--- public/src/pages/discover/discover.js
+++ public/src/pages/discover/discover.js
@@ -1,3 +1,9 @@
+//Função abrir menu no mobile
+const anchorEl = document.body.querySelector('#mobile-nav-menu');
+const menuEl = document.body.querySelector('#usage-menu');
+
+anchorEl.addEventListener('click', () => {menuEl.open = !menuEl.open;});
+
 // Função genérica para abrir modal
 function openModal(modalId){
     const modal = document.getElementById(modalId);
@@ -9,3 +15,4 @@ function openModal(modalId){
         }
     })
 }
+
