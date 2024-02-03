6aeba6b0837003b2a401ee80ae9f835474794c61
Kauã Steliano <stelianok@gmail.com>
2024-02-03 15:35:32 -0300

    Removed unecessary code.

--- index.js
+++ index.js
@@ -19,37 +19,3 @@ import '@material/web/menu/menu-item.js';
 import '@material/web/dialog/dialog.js';
 
 import '@material/web/divider/divider.js';
-
-// Define protected routes 
-const protectedRoutes = {
-  '/discover/index.html': true,
-  '/my-projects/index.html': true,
-};
-
-// Check if the user is authenticated 
-function isAuthenticated() {
-  // Check if the user is authenticated 
-  const token = localStorage.getItem('authToken');
-  if (!token) {
-    // User is not authenticated, redirect to login page 
-    window.location.href = '/login.html';
-  }
-}
-
-// Handle routing 
-function router() {
-  // Get the requested route path 
-  const path = window.location.pathname;
-
-  // Check if the route is protected 
-  const isProtected = Object.keys(protectedRoutes).includes(path);
-
-  // Check if the user is authenticated for protected routes 
-  if (isProtected) {
-    isAuthenticated();
-  }
-}
-
-// Call router on page load and URL change 
-window.onload = router();
-window.onpopstate = router(); 
