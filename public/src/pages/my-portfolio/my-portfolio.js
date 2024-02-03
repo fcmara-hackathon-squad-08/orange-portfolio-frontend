8a8b8acaf4b98bde7483407458547e7eac45cd09
Kauã Steliano <stelianok@gmail.com>
2024-02-03 15:36:35 -0300

    Created function to return user data and save it on localStorage (only if it isn't saved yet.) Created function to return project data.

--- public/src/pages/my-portfolio/my-portfolio.js
+++ public/src/pages/my-portfolio/my-portfolio.js
@@ -24,15 +24,15 @@ function submitProject() {
   toggleModal('success-modal', true)
 }
 
-function toggleModal(modalId, state){
+function toggleModal(modalId, state) {
   const modal = document.getElementById(modalId);
 
-  if(!modal){
+  if (!modal) {
     console.error(`Couldn't find modal with id ${modalId}`);
     return;
   }
 
-  if(state){
+  if (state) {
     modal.setAttribute('open', true);
   }
   else {
@@ -53,3 +53,104 @@ function uploadImage(input) {
   }
 }
 
+/**
+ * Verificar se o usuário está autenticado
+  * Se ele não estiver, mandar ele para /login
+  * Se ele estiver, buscar dados da rota de projects/me
+  * 
+  * Enviar dados para a navBar
+  * 
+  * Alterar dados no Perfil
+  * 
+  * Se o usuário não tiver nenhum projeto renderizar placeholder
+  * Se o usuário tiver projeto, renderizá-los no project-grid
+  * 
+  * Ao clicar no projeto, popular project-preview com dados do projeto
+  * 
+  * Criar função de adicionar projetos pelo addProjectModal
+ */
+async function getUserData() {
+  const apiURL = `https://sq8-orange-fcamra.onrender.com/user/me`;
+  const token = localStorage.getItem('token');
+
+  let requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  }
+
+  try {
+    const response = await fetch(apiURL, requestOptions);
+    const data = await response.json();
+
+    return data;
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+
+}
+
+async function getProjectsData() {
+  const apiURL = `https://sq8-orange-fcamra.onrender.com/project/list/tags/user`;
+  const token = localStorage.getItem('token');
+
+  let requestOptions = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+  }
+
+  try {
+    const response = await fetch(apiURL, requestOptions);
+    const data = await response.json();
+
+    return data;
+  }
+  catch (err) {
+    throw new Error(err);
+  }
+
+}
+
+function setUserDataOnLocalStorage() {
+  getUserData().then((user) => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }).catch((err) => {
+    console.error(err);
+  })
+}
+
+function isUserDataOnLocalStorage() {
+  if (localStorage.getItem('user')) {
+    return true;
+  }
+  return false;
+}
+function isAuthenticated() {
+  // Check if the user is authenticated 
+  const token = localStorage.getItem('token');
+  if (!token) {
+    // User is not authenticated, redirect to login page 
+    window.location.href = '../login/index.html';
+  }
+
+  /**
+   * Se os dados do usuário não estão inseridos ainda, buscar na API
+   * Salvar dados no localStorage
+   * Definir dados na página.
+   */
+
+  if (!isUserDataOnLocalStorage()) {
+    console.log("User not saved on local storage, saving now");
+    setUserDataOnLocalStorage();
+  }
+
+  console.log("User is authenticated!");
+}
+
+isAuthenticated();
